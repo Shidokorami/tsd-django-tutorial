294480336c0d50472d3a03fff461a09c5e02e2b6
Shidokorami <ZawiszaSolec@gmail.com>
2026-06-02 02:57:46 +0200

    add better hints

--- movies/tasks/task2.py
+++ movies/tasks/task2.py
@@ -9,11 +9,11 @@ def get_movies_by_director_last_name(*, last_name: str):
 def get_directors_with_movie_rating_at_least(*, min_rating: float):
     # TODO: Task 2.3 - Create function that returns first and last names of directors
     # who have at least one movie with rating greater than or equal to min_rating.
-    # Remember to avoid duplicates
+    # Remember to avoid duplicates.
     raise NotImplementedError
 
 
 def get_movie_titles_by_director_id(*, director_id: int):
     # TODO: Task 2.4 - Create function that returns movie titles for the given director.
-    # Hint: Use select_related to avoid unnecessary queries.
+    # Hint: Use select_related to avoid unnecessary queries. Use flat=true
     raise NotImplementedError
--- movies/urls.py
+++ movies/urls.py
@@ -2,9 +2,9 @@ from rest_framework.routers import DefaultRouter
 
 from movies.views import MovieViewSet
 
-router = DefaultRouter()
 
 # TODO: Task 3.5 - Register MovieViewSet on the router
+# router = ...
 # router.register(...)
 
-urlpatterns = router.urls
+# urlpatterns = router.urls
